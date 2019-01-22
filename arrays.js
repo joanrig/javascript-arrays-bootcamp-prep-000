5c4d287eb46f468a267037175b090f61b8ad0fa5
Joan in DC <joanrigdon@gmail.com>
2019-01-22 05:54:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,9 +4,14 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 
+// function destructivelyAddElementToBeginningOfArray(array, element){
+//   array.unshift(element);
+//   return array;
+// }
+
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
-  return array;
+  return array
 }
 
 function addElementToEndOfArray(array, element){
